016f8746c3f4a416c37465ef7965c6a551ee79d4
ValeriiaZavadetska <valeria@sweetpea.care>
2023-12-30 18:57:59 +0200

    added options conralloer

--- controller.js
+++ controller.js
@@ -7,3 +7,9 @@ export const postController = (req, res, payload) => {
   res.setHeader('content-type', 'application/json');
   res.end(JSON.stringify({ user: payload }));
 };
+
+export const optionsController = (req, res) => {
+  res.setHeader('Allow', 'OPTIONS, GET, POST');
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('OK');
+};
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { router } from './router.js';
 import { createHttpServer } from './http-server.js';
-import { getController, postController } from './controller.js';
+import { getController, postController, optionsController } from './controller.js';
 
 const PORT = 3001;
 
 router.addRoute('/example', 'GET', getController);
 router.addRoute('/example', 'POST', postController);
+router.addRoute('/example', 'OPTIONS', optionsController);
 
 createHttpServer(router.getRoutes(), PORT);
